6bf978a4b83997d47a346ecb9a47b3e6ba7ef6f0
Haki Bejtullahi <haki.bejtullahi14.95@gmail.com>
2023-01-08 22:32:35 +0100

    first Commit
    123

---
+++ js/index.js
@@ -0,0 +1,35 @@
+import projects from './projects.js';
+
+const navToggleBtn = document.querySelector('.nav-top .btn-icon');
+const navLinks = document.querySelector('.nav-links');
+
+navToggleBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('nav-links-show');
+});
+
+const projectsContainer = document.querySelector('.projects-container');
+
+for (let i = 0; i < projects.length; i++) {
+  const name = projects[i].name;
+  const id = projects[i].id;
+  const imgPath = projects[i].imgPath;
+  const url = projects[i].url;
+  const languages = projects[i].languages;
+
+  projectsContainer.innerHTML += `        <a href="${url}" class="project-box" id='${id}'>
+          <h4 class="project-title">${name}</h4>
+          <img
+            src="${imgPath}"
+            alt="${name}"
+          />
+          <div class="box-footer">
+            <ul class="projects-lang">
+             ${languages
+               .map((item) => {
+                 return `<li>${item}</li>`;
+               })
+               .join('')}
+            </ul>
+          </div>
+        </a>`;
+}
---
+++ js/projects.js
@@ -0,0 +1,46 @@
+const projects = [
+  {
+    id: 'project01',
+    name: 'tea station',
+    url: 'https://teastationprojecthaki.netlify.app',
+    languages: ['js', 'html', 'css'],
+    imgPath: './plugins/projectImages/project001.png',
+  },
+  {
+    id: 'project02',
+    name: 'shpx',
+    url: 'https://bitacademyprojecthaki.netlify.app',
+    languages: ['js', 'html', 'css', 'bootstrap'],
+    imgPath: './plugins/projectImages/project002.png',
+  },
+  {
+    id: 'project03',
+    name: 'Backroad Tour',
+    url: 'https://backroadtourcompanyhaki.netlify.app',
+    languages: ['react js'],
+    imgPath: './plugins/projectImages/project003.png',
+  },
+  {
+    id: 'project04',
+    name: 'color hex generator',
+    url: 'https://colorgeneratorreacthaki.netlify.app',
+    languages: ['react js'],
+    imgPath: './plugins/projectImages/project004.png',
+  },
+  {
+    id: 'project05',
+    name: 'Vip Scent',
+    url: 'https://universumproject.netlify.app',
+    languages: ['html', 'css', 'js'],
+    imgPath: './plugins/projectImages/project005.png',
+  },
+  {
+    id: 'project06',
+    name: 'Markdown Preview',
+    url: 'https://markdownpreviewreact.netlify.app',
+    languages: ['react js'],
+    imgPath: './plugins/projectImages/project006.png',
+  },
+];
+
+export default projects;
