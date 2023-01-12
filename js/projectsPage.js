7ac84bb92043a3d7779d89ff9d72bcab81656083
Haki Bejtullahi <haki.bejtullahi14.95@gmail.com>
2023-01-12 22:43:45 +0100

    search input

---
+++ js/projectsPage.js
@@ -0,0 +1,55 @@
+import projects from './projects.js';
+
+const searchInput = document.querySelector('#searchForm input');
+
+searchInput.addEventListener('keyup', (e) => {
+  const searchValue = e.currentTarget.value;
+  const newProjects = projects.filter((item) =>
+    item.name.includes(searchValue)
+  );
+
+  document.querySelector('.projects-container').innerHTML = newProjects
+    .map((item) => {
+      const { url, id, name, imgPath, languages } = item;
+
+      return `        <a href="${url}" class="project-box" id='${id}' target="_blank">
+   <h4 class="project-title">${name}</h4>
+   <img
+   src="${imgPath}"
+   alt="${name}"
+   />
+            <div class="box-footer">
+            <ul class="projects-lang">
+            ${languages
+              .map((item) => {
+                return `<li>${item}</li>`;
+              })
+              .join('')}
+             </ul>
+             </div>
+             </a>`;
+    })
+    .join('');
+  if (searchValue.trim() === '') {
+    projects.map((item) => {
+      const { url, id, name, imgPath, languages } = item;
+
+      return `        <a href="${url}" class="project-box" id='${id}' target="_blank">
+   <h4 class="project-title">${name}</h4>
+   <img
+   src="${imgPath}"
+   alt="${name}"
+   />
+            <div class="box-footer">
+            <ul class="projects-lang">
+            ${languages
+              .map((item) => {
+                return `<li>${item}</li>`;
+              })
+              .join('')}
+             </ul>
+             </div>
+             </a>`;
+    });
+  }
+});
